f12b38cf16a8ca3aadc73b4903e902ea1540d82b
YasaswiKapila19 <yasaswi.kapila@gmail.com>
2025-10-19 06:37:08 +0000

    updating test_val.py

--- tests/test_val.py
+++ tests/test_val.py
@@ -3,5 +3,5 @@ from validate import validate_data
 
 def test_validate_data_passes():
     result = validate_data("data/test.csv")
-    assert result is True, "Size < 10, Data validation should pass for clean data"
+    assert result is True, "Data validation should pass for clean data"
 
