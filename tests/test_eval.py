901b9975e86876c691111f09b7fb8df9395342b1
YasaswiKapila19 <yasaswi.kapila@gmail.com>
2025-10-19 12:34:27 +0000

    Changes to test_eval.py

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -9,3 +9,7 @@ def test_accuracy_threshold():
     metrics = evaluate_model("data/test.csv", "model.pkl")
     assert metrics["accuracy"] > 0.7, "Accuracy should be above 70%"
 
+def test_precision_threshold():
+    metrics = evalute_model("data/test.csv","model.pkl")
+    assert metrics["precision"]>0.7, "Precision should be greater than 0.7"
+
